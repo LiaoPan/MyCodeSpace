89ae82b68cc435daee38700905ab29ddda03b3ef
LiaoPan <LiaoPan_2015@163.com>
2015-08-31 20:34:43 +0800

    recall Java

---
+++ EnhanceJava/ex001/src/ex001/ex2_5.java
@@ -0,0 +1,18 @@
+package ex001;
+
+public class ex2_5 {
+	int age =23;  //成员变量
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int var_1 = 2;
+		int var_2 = 3;
+		System.out.println("1+1="+(1+1));
+		System.out.println("1-1="+(1-1));
+		System.out.println(var_1+var_2);
+		int age = 12;  //局部变量
+		System.out.println("局部变量值"+age);
+		double length = 1.000;
+		System.out.println("value = "+(int)length);
+	}
+
+}
