95505494ab16c54ce43ab60f86f23ab2b6465977
LiaoPan <LiaoPan_2015@163.com>
2015-09-17 10:33:27 +0800

    netbeans for python

---
+++ EnhancePython/HelloWorldPython_ubuntu/src/helloworldpython_ubuntu.py
@@ -0,0 +1,13 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+import sys
+import threading
+
+__author__ = "reallocing"
+__date__ = "$Sep 17, 2015 10:19:14 AM$"
+
+if __name__ == "__main__":
+    print "Hello World"
+
+print "Hello World"
---
+++ EnhancePython/ubuntuTry.py
@@ -0,0 +1,7 @@
+#coding:utf-8
+#author:L.P
+#time:201509152050
+import threading
+
+print "play with ubuntu"
+print "atuo completion"
