b8f86828271dc11fb61743030c18f6d803910052
LiaoPan <LiaoPan_2015@163.com>
2015-11-12 10:02:13 +0800

    TestWrapper

---
+++ EnhanceJava/TestWrapper/src/TestWrapper.java
@@ -0,0 +1,26 @@
+
+public class TestWrapper {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//No.1 
+				//开始写代码。创建几个简单类型封装类的对象
+		Integer i = new Integer(10);
+		Integer j = new Integer(10);
+		Double dbl1 = new Double(123);
+		Double dbl2 = new Double(123);
+		Boolean bln = new Boolean(true);
+		Character chr = new Character('a');//debug:using "a" will throw error;
+		//end code
+		System.out.println(i==j);
+		int m = 10;
+		int n = 10;
+		System.out.println(m==n);
+				
+		
+	}
+
+}
